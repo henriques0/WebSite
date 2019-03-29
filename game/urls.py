6113efa184d1990145db6013a93ac8e52e4afbd0
henriques0 <32529883+henriques0@users.noreply.github.com>
2019-03-29 20:53:11 -0300

    Template
    Alteração do Template do site

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', PaginaInicialView.as_view(), name="index"),
+    path('sobre', PaginaSobreView.as_view(), name="sobre"),
+    path('contao', PaginaContatoView.as_view(), name="contato"),
 
 ]
--- game/views.py
+++ game/views.py
@@ -7,3 +7,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class PaginaInicialView(TemplateView):
 	template_name = "index.html"
+
+class PaginaSobreView(TemplateView):
+		template_name = "sobre.html"
+
+class PaginaContatoView(TemplateView):
+		template_name = "contato.html"
